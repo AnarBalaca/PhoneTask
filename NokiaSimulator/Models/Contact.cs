7b065b502ef2282c3b73f849d00b4b35ba40faa8
Mranar <anarbalacayev@gmail.com>
2022-12-29 06:27:54 +0400

    Created HOmeWork

---
+++ NokiaSimulator/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NokiaSimulator.Models
+{
+    public class Contact
+    {
+        private readonly int _Id = 0;
+        
+        public int Id { get => +_Id;  }
+        public string? FullName { get; set; }
+        public int Number { get; set; }
+
+        public Contact(string Name , int Num)
+        {
+            FullName = Name;
+            Number = Num;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ NokiaSimulator/Models/Phone.cs
@@ -0,0 +1,82 @@
+﻿using NokiaSimulator.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NokiaSimulator.Models
+{
+    public class Phone
+    {
+        public int Number { get; set; }
+        public Provider Providers { get; set; }
+        public decimal Balance { get; set; }
+        public ICollection<Contact>? ContactList { get; set; }
+
+
+
+
+
+        public void AddContact(string contactName , int contactNum)
+        {
+            Contact contact = new Contact(contactName , contactNum);
+
+
+            ContactList.Add(contact);
+
+        }
+
+
+        public void GetContactList()
+        {
+            Exception exception = new Exception("NULL SIUUUUU!");
+
+            if (ContactList is null)
+            {
+                throw exception;
+            }
+            else
+            {
+                foreach (var contact in ContactList)
+                {
+                    Console.WriteLine(contact);
+
+                }
+            }
+        }
+
+
+
+        public void AddBalance(decimal balance)
+        {
+            Balance += balance;
+            Console.WriteLine(Balance);
+        }
+
+
+        public void ChooseProvider()
+        {
+            Console.WriteLine($"1:{Provider.Nar}:\n" +
+                              $"2:{Provider.Azercell} \n" +
+                              $"3:{Provider.Bakcell}");
+            
+            
+
+        }
+
+        public void PhoneCall()
+        {
+            
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
